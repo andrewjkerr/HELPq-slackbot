deb330357e581013442e81794bd7bd53e65be06b
Andrew Kerr <me@andrewjkerr.com>
2017-01-08 23:45:39 -0500

    It lives!
    I would like to:
    1. Add proper logging (heh)
    2. Add a webhook option for HELPq to reach out to a user when a mentor
    has claimed their ticket.

--- README.md
+++ README.md
@@ -1,14 +1,53 @@
-## slackbot-ruby-starter
+# HELPq-slackbot
 
-A starter kit for writing Slackbots in Ruby!
+A Slackbot that interfaces with [Edwin Zhang's HELPq](https://github.com/ehzhang/HELPq)! Written for SwampHacks 2017.
 
-## Usage
+## End-User Guide
 
-¯\\\_(ツ)_/¯
+### Commands
 
-## Installing
+This bot is fairly easy to use. You have the following commands:
+- `.ticket create [topic]`: creates a ticket for mentors to see
+- `.ticket delete`: deletes your ticket
+- `.ticket get`: fetches your open ticket
+- `.ticket help`: view these commands
+- `.ticket [anything-else]`: fetches your open ticket
 
-1. Clone this respository `git clone https://github.com/andrewjkerr/slackbot-ruby-starter.git`
+Due to requirements on HELPq, you are only allowed one ticket at a time. If you need to re-submit your ticket, please delete your ticket with `.ticket delete` and create a new one.
+
+### Got an Error?
+
+Please tell the admin of your Slack to check the logs and submit a ticket here! Thanks. :)
+
+## Setting up HELPq-slackbot
+
+### Requirements
+
+- A special version of HELPq with an API that can be found here: [https://github.com/andrewjkerr/HELPq](https://github.com/andrewjkerr/HELPq).
+- Ruby 2.3.0
+- Bundler (`gem install bundler`)
+- A Slack bot key!
+- A HELPq special version API key that you will need to set in your special version of HELPq's config file.
+
+### Installing
+
+1. Clone this respository `git clone https://github.com/andrewjkerr/HELPq-slackbot.git`
 2. `bundle install`
 3. Rename `config/application.sample.yml` to `config/application.yml` and add your values
 4. `ruby bot.rb`
+
+## License
+
+This repository is licensed under the [MIT license](https://github.com/andrewjkerr/HELPq-slackbot/blob/master/LICENSE.md).
+
+## Contributing
+
+Want to contribute? Great! Here's what you do:
+
+1. Fork this repository
+2. Push some code to your fork
+3. Come back to this repository and open a PR
+4. After some review, get that PR merged to master
+5. Give yourself a pat on the back; you're awesome!
+
+Feel free to also open an issue with any requests!
--- bot.rb
+++ bot.rb
@@ -1,5 +1,6 @@
 require 'slack-ruby-client'
 require 'figaro'
+require 'httparty'
 
 def figaro_init
   Figaro.application = Figaro::Application.new(environment: 'production', path: 'config/application.yml')
@@ -11,22 +12,174 @@ def slack_init
   Slack.configure { |config| config.token = ENV['SLACK_API_TOKEN'] }
 end
 
+def create_ticket(name, topic, msg_data)
+    if topic == ''
+        @real_time_client.message(channel: msg_data['channel'], text: 'You need to enter a topic! Please see `.ticket help` for more information.')
+        return false
+    end
+
+    url = "#{ENV['HELPQ_URL']}/createTicket"
+
+    options = {
+        query: {
+            apiKey: ENV['HELPQ_API_KEY'],
+            name: name,
+            topic: topic
+        }
+    }
+
+    resp = HTTParty.get(url, options)
+
+    return handle_error(name, resp, msg_data) unless resp.code == 200
+
+    msg = 'Your ticket has been created! A mentor will be with you shortly.'
+    @real_time_client.message(channel: msg_data['channel'], text: msg)
+
+    puts "Successful create for #{name}: #{resp.body}"
+
+    return true
+end
+
+def delete_ticket(name, msg_data)
+    url = "#{ENV['HELPQ_URL']}/deleteTicket"
+
+    options = {
+        query: {
+            apiKey: ENV['HELPQ_API_KEY'],
+            name: name
+        }
+    }
+
+    resp = HTTParty.get(url, options)
+
+    return handle_error(name, resp, msg_data) unless resp.code == 200
+
+    msg = 'Your ticket has been successfully deleted.'
+    @real_time_client.message(channel: msg_data['channel'], text: msg)
+
+    puts "Successful delete for #{name}: #{resp.body}"
+
+    return true
+end
+
+def get_ticket(name, msg_data)
+    url = "#{ENV['HELPQ_URL']}/getTicket"
+
+    options = {
+        query: {
+            apiKey: ENV['HELPQ_API_KEY'],
+            name: name
+        }
+    }
+
+    resp = HTTParty.get(url, options)
+
+    return handle_error(name, resp, msg_data) unless resp.code == 200
+
+    resp_obj = JSON::parse(resp.body)
+
+    msg = "Found a ticket for \"#{resp_obj['ticket']['topic']}\" with a status of #{resp_obj['ticket']['status']}."
+    @real_time_client.message(channel: msg_data['channel'], text: msg)
+
+    puts "Successful get for #{name}: #{resp_obj}"
+
+    return true
+end
+
+def handle_error(name, resp, msg_data)
+    # Parse response to verify errors
+    resp_obj = JSON::parse(resp.body)
+
+    # DEBUG
+    puts "#{resp_obj}"
+
+    # Default error message.
+    msg = 'An error occured. Please contact an admin.'
+
+    # Unauthorized; wrong API key.
+    if resp.code == 401
+        msg = 'The API key is incorrect. Please contact an admin.'
+    end
+
+    # Missing a parameter; either topic or name.
+    if resp.code == 400 && resp_obj['error'] == 'missing_param'
+        msg = "#{resp_obj['msg']}. Please contact an admin."
+    end
+
+    # Already have a ticket open! Need to consult help.
+    if resp.code == 400 && resp_obj['error'] == 'open_ticket'
+        msg = 'You already have a ticket open! Please use `.ticket help` for available options.'
+    end
+
+    # No ticket found.
+    if resp.code == 404 && resp_obj['error'] == 'no_ticket'
+        msg = 'You currently do not have any open tickets. Please create one with `.ticket create [topic]`.'
+    end
+
+    # Some sort of server error...
+    if resp.code == 500
+        msg = resp_obj['msg']
+    end
+
+    @real_time_client.message(channel: msg_data['channel'], text: msg)
+
+    # Puts the error
+    puts "ERROR occured for #{name}: #{msg} // #{resp_obj}"
+
+    return false
+end
+
+def help(msg_data)
+    help_text = %q(
+        This bot is fairly easy to use. You have the following commands:
+        - `.ticket create [topic]`: creates a ticket for mentors to see
+        - `.ticket delete`: deletes your ticket
+        - `.ticket get`: fetches your open ticket
+        - `.ticket [anything-else]`: fetches your open ticket
+    )
+    @real_time_client.message(channel: msg_data['channel'], text: help_text)
+    return true
+end
+
 
 figaro_init
 slack_init
 
-client = Slack::RealTime::Client.new
+client = Slack::Web::Client.new
+@real_time_client = Slack::RealTime::Client.new
 
-client.on :hello do
-  puts "Successfully connected, welcome '#{client.self['name']}' to the '#{client.team['name']}' team at https://#{client.team['domain']}.slack.com."
+@real_time_client.on :hello do
+  puts "Successfully connected, welcome '#{@real_time_client.self['name']}' to the '#{@real_time_client.team['name']}' team at https://#{@real_time_client.team['domain']}.slack.com."
 end
 
-client.on :message do |data|
-  puts data
+@real_time_client.on :message do |data|
+  # Match on `.ticket`
+  if /(^\.ticket)(.*)/.match(data['text'])
+    # Determine the command.
+    command = data['text'].gsub(/^.ticket/, '').strip
 
-  if matchdata = /(^\.test)(.*)/.match(data['text'])
-    client.message(channel: data['channel'], text: "Hello World")
+    # Determine name from user.
+    name = client.users_info(user: data['user'])['user']['name']
+
+    # Need to check if we're creating a ticket.
+    if /(^create)(.*)/.match(command)
+        topic = command.gsub(/^create/, '').strip
+        create_ticket(name, topic, data)
+        next
+    end
+
+    case command
+    when 'get'
+        get_ticket(name, data)
+    when 'delete'
+        delete_ticket(name, data)
+    when 'help'
+        help(data)
+    else
+        # Assume anyone else is looking to get their ticket status...
+        get_ticket(name, data)
+    end
   end
 end
 
-client.start!
+@real_time_client.start!
